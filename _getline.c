e3577628bed0217520592136b4741ba6bb7e7987
Rhid07 <opeyemidurodola007@gmail.com>
2023-12-14 01:49:41 +0100

    prepare_command_args.c

--- _getline.c
+++ _getline.c
@@ -12,40 +12,40 @@
 
 ssize_t read_input(char **ptr, size_t *num)
 {
-        static char buffer[READ_SIZE];
-        static ssize_t bytes_read;
-        ssize_t i;
+	static char buffer[READ_SIZE];
+	static ssize_t bytes_read;
+	ssize_t i;
 
-        if (*num == 0 || *ptr == NULL)
-                return (-1);
+	if (*num == 0 || *ptr == NULL)
+		return (-1);
 
-        bytes_read = read(STDIN_FILENO, buffer, READ_SIZE);
+	bytes_read = read(STDIN_FILENO, buffer, READ_SIZE);
 
-        if (bytes_read == -1)
-        {
-                perror("Read input failed");
-                return (-1);
-        }
-        else if (bytes_read == 0)
-        {
-                return (0);
-        }
+	if (bytes_read == -1)
+	{
+		perror("Read input failed");
+		return (-1);
+	}
+	else if (bytes_read == 0)
+	{
+		return (0);
+	}
 
-        *ptr = malloc(bytes_read + 1);
-        if (*ptr == NULL)
-        {
-                perror("Memory allocation error");
-                return (-1);
-        }
+	*ptr = malloc(bytes_read + 1);
+	if (*ptr == NULL)
+	{
+		perror("Memory allocation error");
+		return (-1);
+	}
 
-        (*ptr)[bytes_read] = '\0';
+	(*ptr)[bytes_read] = '\0';
 
-        for (i = 0; i < bytes_read; ++i)
-        {
-                (*ptr)[i] = buffer[i];
-        }
+	for (i = 0; i < bytes_read; ++i)
+	{
+		(*ptr)[i] = buffer[i];
+	}
 
-        return (bytes_read);
+	return (bytes_read);
 }
 
 /**
@@ -57,38 +57,38 @@ ssize_t read_input(char **ptr, size_t *num)
  **/
 ssize_t _getline(char **ptr, size_t *num)
 {
-        static char buffer[READ_SIZE];
-        static ssize_t bytes_read;
-        size_t i;
-        char *newline_position;
-        ssize_t input_bytes;
+	static char buffer[READ_SIZE];
+	static ssize_t bytes_read;
+	size_t i;
+	char *newline_position;
+	ssize_t input_bytes;
 
-        input_bytes = read_input(ptr, num);
-        if (input_bytes <= 0)
-                return (input_bytes);
+	input_bytes = read_input(ptr, num);
+	if (input_bytes <= 0)
+		return (input_bytes);
 
-        if (bytes_read == 0)
-                i = 0;
+	if (bytes_read == 0)
+		i = 0;
 
-        newline_position = buffer + i;
+	newline_position = buffer + i;
 
-        while (i < (size_t)input_bytes && buffer[i] != '\n')
-        {
-                ++i;
-        }
+	while (i < (size_t)input_bytes && buffer[i] != '\n')
+	{
+		++i;
+	}
 
-        bytes_read = i;
+	bytes_read = i;
 
-        free(*ptr);
-        *ptr = malloc(bytes_read + 1);
-        if (*ptr == NULL)
-                return (-1);
+	free(*ptr);
+	*ptr = malloc(bytes_read + 1);
+	if (*ptr == NULL)
+		return (-1);
 
-        for (i = 0; i < (size_t)bytes_read; ++i)
-        {
-                (*ptr)[i] = *newline_position++;
-        }
-        (*ptr)[bytes_read] = '\0';
+	for (i = 0; i < (size_t)bytes_read; ++i)
+	{
+		(*ptr)[i] = *newline_position++;
+	}
+	(*ptr)[bytes_read] = '\0';
 
-        return (bytes_read + 1);
+	return (bytes_read + 1);
 }
