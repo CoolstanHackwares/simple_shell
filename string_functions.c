9ce642e1f3bbe6041ec8fe14e14b4644524cdf2c
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-12-15 16:40:32 -0800

    atoi

--- string_functions.c
+++ string_functions.c
@@ -94,22 +94,23 @@ char *_strtok(char *s, const char *del)
  **/
 
 int _atoi(char *s)
-{
-	int a, b, n, x;
+{	
+	int a, n, x;
 
 	a = n = 0;
 	x = 1;
-	while ((s[a] < '0' || s[a] > '9') && (s[a] != '\0'))
+
+	while (s[a] != '\0')
 	{
 		if (s[a] == '-')
+		{
 			x *= -1;
+		}
+		else if (s[a] >= '0' && s[a] <= '9')
+		{
+			n = (n * 10) + x * ((s[a]) - '0');
+		}
 		a++;
-	}
-	b = a;
-	while ((s[b] >= '0') && (s[b] <= '9'))
-	{
-		n = (n * 10) + x * ((s[b]) - '0');
-		b++;
 	}
 	return (n);
 }
